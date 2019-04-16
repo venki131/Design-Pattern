c9a54cfaac34479df57125700a4b2c219317266c
venkatesh <venkatesh.n@techtreeit.com>
2019-04-16 12:41:11 +0530

    removed GPS codes.

--- app/src/main/java/com/example/learningrd/MainActivity.java
+++ app/src/main/java/com/example/learningrd/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initUi();
     }
 
@@ -39,43 +38,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnMvc.setOnClickListener(this);
         btnMvp.setOnClickListener(this);
         btnMvvm.setOnClickListener(this);
-    }
-
-    public void asyncTaskDemo(final String message) {
-        AsyncTask<Void, Void, Boolean> sendThread = new AsyncTask<Void, Void, Boolean>() {
-            @Override
-            protected Boolean doInBackground(Void... voids) {
-
-                Log.d("Connection Service", "Sending : " + message);
-                return null;
-            }
-        };
-        sendThread.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-    }
-
-    private void turnGPSOn() {
-        String provider = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
-
-        if (!provider.contains("gps")) { //if gps is disabled
-            final Intent poke = new Intent();
-            poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
-            poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
-            poke.setData(Uri.parse("3"));
-            sendBroadcast(poke);
-            Log.d("inside GPS", "true");
-        }
-    }
-
-    private void turnGPSOff() {
-        String provider = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
-
-        if (provider.contains("gps")) { //if gps is enabled
-            final Intent poke = new Intent();
-            poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
-            poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
-            poke.setData(Uri.parse("3"));
-            sendBroadcast(poke);
-        }
     }
 
     @Override
